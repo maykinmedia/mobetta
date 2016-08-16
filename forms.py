a58bf2a7571b6363e7dfa24abf1f5da77dab2a10
Ben Wadsworth <ben@maykinmedia.nl>
2016-08-16 12:40:26 +0200

    Added 'fuzzy' checkbox to form

--- forms.py
+++ forms.py
@@ -3,3 +3,4 @@ from django import forms
 class TranslationForm(forms.Form):
     msgid = forms.CharField(max_length=1024, widget=forms.TextInput(attrs={'readonly': 'readonly'}))
     translation = forms.CharField(max_length=1024)
+    fuzzy = forms.BooleanField()
