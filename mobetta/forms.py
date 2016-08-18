14278986ba4e107c8753386b0cfdf8de7f961fad
Ben Wadsworth <ben@maykinmedia.nl>
2016-08-18 12:09:02 +0200

    Cleaned up template inheritance for file_detail.html so that most of the rendering is taken care of by the superclass

--- mobetta/forms.py
+++ mobetta/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class TranslationForm(forms.Form):
-    msgid = forms.CharField(max_length=1024, widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    translation = forms.CharField(max_length=1024, required=False)
+    msgid = forms.CharField(max_length=1024, widget=forms.TextInput(attrs={'readonly': 'readonly', 'size': '80'}))
+    translation = forms.CharField(max_length=1024, widget=forms.TextInput(attrs={'size': '80'}), required=False)
     old_translation = forms.CharField(max_length=1024, widget=forms.HiddenInput(), required=False)
     fuzzy = forms.BooleanField(required=False)
     old_fuzzy = forms.BooleanField(required=False, widget=forms.HiddenInput())
