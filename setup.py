2a114427249dc2da4ea0c715984b9a5d3b26a152
Sergei Maertens <sergei@maykinmedia.nl>
2016-12-09 11:37:09 +0100

    update meta information

--- setup.py
+++ setup.py
@@ -13,7 +13,11 @@ setup(
     license='MIT',
 
     # packaging
-    install_requires=['Django>=1.8','polib', 'djangorestframework'],
+    install_requires=[
+        'Django>=1.8',
+        'polib',
+        'djangorestframework'
+    ],
     include_package_data=True,
     packages=find_packages(),
 
@@ -30,14 +34,19 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',  # example license
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
