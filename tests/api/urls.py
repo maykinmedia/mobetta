10231303c0a262973c24c2adfb7de847a5eb77e5
Sergei Maertens <sergei@maykinmedia.nl>
2016-12-09 11:49:05 +0100

    Reproduce #46

---
+++ tests/api/urls.py
@@ -0,0 +1,2 @@
+# dummy urls module for namespace test
+urlpatterns = []
--- tests/urls.py
+++ tests/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include('tests.api.urls', namespace='api')),  # See #46
     url(r'^mobetta/', include('mobetta.urls')),
 ]
