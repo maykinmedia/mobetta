8089e13fcb9fe09160fdf1c52d716bd4e24e8148
Sergei Maertens <sergei@maykinmedia.nl>
2017-05-18 09:51:27 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='mobetta',
-    version='0.1.5',
+    version='0.2.0',
     license='BSD',
 
     # packaging
