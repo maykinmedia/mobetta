71c56d7e68c8b71eb247a9851fac7db2dc87e200
Sergei Maertens <sergei@maykinmedia.nl>
2017-05-18 16:42:11 +0200

    Set up docs

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ import os
 
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+basedir = os.path.normpath(os.path.abspath(os.path.dirname(__file__)))
+
+
+with open(os.path.join(basedir, 'docs', 'index.rst')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+os.chdir(basedir)
 
 setup(
     name='mobetta',
@@ -53,12 +56,13 @@ setup(
     author='Maykin Media, Ben Wadsworth',
     author_email='ben@maykinmedia.nl',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
