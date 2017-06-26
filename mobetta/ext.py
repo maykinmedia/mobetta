9c32518254c4b16970eb3d92e7b94842afc79e90
Sergei Maertens <sergei@maykinmedia.nl>
2017-06-26 18:42:02 +0200

    Fixes #18 -- don't load ICU models if extension isn't installed

---
+++ mobetta/ext.py
@@ -0,0 +1,8 @@
+from django.apps import apps
+
+
+if apps.is_installed('mobetta.icu'):
+    from .icu.mixins import ICULanguageListMixin
+else:
+    class ICULanguageListMixin(object):
+        pass
